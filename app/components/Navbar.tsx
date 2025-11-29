0581bbc1c749f684d07419540367be431a80e5e0
Karm Pandya <KarmPandya007@users.noreply.github.com>
2025-11-29 14:16:59 +0530

    Deployment Changes Done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
+import photo from "../assets/large.png";
 
 export default function Navbar() {
     return (
         <div className="bg-teal-600 text-white flex flex-row justify-between items-center px-20 py-4 shadow-lg sticky top-0 z-50">
-            <div className="logo text-3xl animate-fadeIn hover:scale-110 transition cursor-pointer">
+            <div className="logo animate-fadeIn hover:scale-110 transition cursor-pointer">
 
-                <Link href="/" className="nav-link animate-fadeIn">BookHive</Link>
+                <Link href="/" className="nav-link animate-fadeIn">
+                    <img src={photo.src} alt="BookHive" className="scale-130 h-10 w-15" />
+                </Link>
             </div>
             <div className="nav flex flex-row gap-6 text-lg">
                 <Link href="/addbook" className="nav-link animate-fadeIn">Add Book</Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@
 import "./globals.css";
 import type { Metadata } from 'next';
 import Navbar from './components/Navbar';
+import photo from "./assets/large.png";
 
 export const metadata: Metadata = {
   title: "BookHive - Library Management System",
   description: "A comprehensive library management system for managing books and resources",
+  icons: {
+    icon: photo.src,
+  },
 };
 
 export default function RootLayout({
