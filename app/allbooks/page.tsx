435f3d62a6453b71723c69adc56a43c27cf1bba5
Karm Pandya <KarmPandya007@users.noreply.github.com>
2025-12-10 14:19:26 +0530

    Updates the API route

--- app/allbooks/page.tsx
+++ app/allbooks/page.tsx
@@ -6,7 +6,7 @@ export default function AllBooks() {
   const [books, setBooks] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:7000/api/books")
+    fetch("https://bookhive-library-management-system-ir5q.onrender.com")
       .then(res => res.json())
       .then(data => setBooks(data.allBooks || []))
       .catch(error => console.log(error));
